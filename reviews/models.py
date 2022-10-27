eb293d420c2e146ecffd3e2116a0dda419730ed6
MoonSanghee <psycho13th@gmail.com>
2022-10-27 11:53:30 +0900

    fit pjt fit apps name accounts, reviews

---
+++ reviews/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = ProcessedImageField(upload_to='images/', blank=True,
+                                processors=[ResizeToFill(1200, 960)],
+                                format='JPEG',
+                                options={'quality': 80})
+    like_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
